07fa32f9a30ec3a175b1c34fc5ca711885761df0
Adeyanju Adeyemi <alex.olaolu007@gmail.com>
2022-08-25 09:36:22 -0700

    Added Fcm Token

--- src/App.js
+++ src/App.js
@@ -1,34 +1,43 @@
 import GoogleButton from 'react-google-button'
 import { signInWithPopup } from "firebase/auth";
-import { auth } from './firebase.js'
+import { auth, getToken, messaging, deleteToken } from './firebase.js'
 import { GoogleAuthProvider } from "firebase/auth";
 import { useEffect, useState } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
-  const [userToken, setUserToken] = useState(null); // userToken
-  const [label, setLabel] = useState('Sign In With Google') // 
+  const [userGoogleToken, setUserGoogleToken] = useState(null); // userGoogleToken
+  const [userFcmToken, setUserFcmToken] = useState(null); // userFcmToken
+  const [googleLabel, setGoogleLabel] = useState('Sign In With Google') // Google button label
+  const [fcmLabel, setFcmLabel] = useState("Get FCM Token") // FCM button label
+  const [fcmLoading, setFcmLoading] = useState(false) // FCM loading state
+  const [googleLoading, setGoogleLoading] = useState(false) // FCM loading state
+
+
   const googleOnClick = () => {
-    if (userToken) { // Log out if there is a user
-      setUserToken(null)
-      setLabel('Sign In With Google');
-      toast.success("Bye Bye")
+    if (userGoogleToken) { // Log out if there is a user
+      setUserGoogleToken(null)
+      setGoogleLabel('Sign In With Google');
+      toast.success("Google says Bye Bye")
       localStorage.removeItem('koredeGoogleAuth')
     } else { // Sign in user
+      setGoogleLoading(true)
       const provider = new GoogleAuthProvider();
       signInWithPopup(auth, provider)
         .then((result) => {
           // This gives you a Google Access Token. You can use it to access the Google API.
           localStorage.setItem('koredeGoogleAuth', result.user.accessToken)
-          setUserToken(result.user.accessToken)
+          setUserGoogleToken(result.user.accessToken)
           toast.success("Login Succesfully")
-          setLabel('Sign Out')
+          setGoogleLabel('Sign Out')
+          setGoogleLoading(false)
           console.log(result.user) // user infos
         }).catch((error) => {
           // Handle Errors here.
           const errorCode = error.code;
           const errorMessage = error.message;
+          setGoogleLoading(false);
           alert(`An Error occured whie signing with google, error code is ${errorCode} and the error message is ${errorMessage}`)
           // ...
         });
@@ -36,7 +45,45 @@ function App() {
 
   }
 
-  const copyToClipboard = (e) => {
+  const getFcmToken = async () => {
+    if (!userFcmToken) { // get a new fcm token if there is none
+      try {
+        setFcmLoading(true);
+        const token = await getToken(messaging, { vapidKey: process.env.NEXT_PUBLIC_VAPID_KEY })
+        if (token) {
+          toast.success("FCM Token Retrieved Successfully")
+          setFcmLabel("Delete Fcm Token")
+          setUserFcmToken(token)
+          localStorage.setItem('koredeFCMAuth', token)
+        } else {
+          toast.error('Ooop, Your didnt grant access to get fcm token')
+        }
+        setFcmLoading(false);
+      } catch (err) {
+        toast.error(err.message)
+        console.log(err)
+        setFcmLoading(false);
+      }
+    } else { // delete the fcm token if there is one
+      try {
+        setFcmLoading(true);
+        await deleteToken(messaging)
+        setFcmLabel("Get New FCM Token");
+        setUserFcmToken(null);
+        toast.success("FCM Token Delete Successfully")
+        localStorage.removeItem("koredeFCMAuth")
+        setFcmLoading(false);
+      } catch (err) {
+        toast.error("An Error occurred while deleteing FCM Token")
+        setFcmLoading(false);
+        console.log(err);
+      }
+
+    }
+
+  }
+
+  const copyToClipboard = (e, type) => {
     /* Get the text */
     let copiedText = e.currentTarget.innerText
 
@@ -44,15 +91,24 @@ function App() {
     /* Copy the text inside the text field */
     navigator.clipboard.writeText(copiedText);
 
-    toast.success("Token Copied!!")
+    toast.success(`${type} Token Copied!!`)
 
   }
   useEffect(() => { // keep user consisitent in local storage
     if (localStorage.koredeGoogleAuth) {
       const token = localStorage.getItem('koredeGoogleAuth')
-      setUserToken(token)
-      console.log(token)
-      setLabel('Sign Out')
+      setUserGoogleToken(token)
+      setGoogleLabel('Sign Out')
+    } else if (localStorage.koredeFCMAuth) {
+      const token = localStorage.getItem('koredeFCMAuth')
+      setUserFcmToken(token)
+      setFcmLabel("Delete Fcm Token")
+    }
+
+    return () => {
+      //clear local storage when component is unmounting
+      localStorage.removeItem("koredeGoogleAuth")
+      localStorage.removeItem("koredeFCMAuth")
     }
   }, [])
 
@@ -61,14 +117,26 @@ function App() {
       <ToastContainer autoClose={'2000'} />
       <div className="text-center flex flex-col justify-center p-10 h-auto space-y-3">
         {
-          userToken && <p> <span>your Token:</span> <span onClick={copyToClipboard} className="font-bold cursor-pointer block break-all">{userToken}</span> </p>
+          userGoogleToken && <p> <span>your Google Token:</span> <span onClick={(e) => copyToClipboard(e, "Google")} className="font-bold cursor-pointer block break-all">{userGoogleToken}</span> </p>
         }
-
         <GoogleButton
-          label={label}
+          label={googleLoading ? "Loading...." : googleLabel}
           className='!mx-auto'
           onClick={googleOnClick}
+          disabled={googleLoading}
         />
+        {
+          userFcmToken && <p> <span>your FCM Token:</span> <span onClick={(e) => copyToClipboard(e, "FCM")} className="font-bold cursor-pointer block break-all">{userFcmToken}</span> </p>
+        }
+        <GoogleButton
+          label={fcmLoading ? "Loading...." : fcmLabel}
+          className='!mx-auto'
+          onClick={getFcmToken}
+          disabled={fcmLoading}
+        />
+
+
+
       </div>
     </>
 
--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getMessaging, getToken, deleteToken } from "firebase/messaging";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -18,7 +19,10 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-initializeApp(firebaseConfig);
-const auth = getAuth();
+const app = initializeApp(firebaseConfig);
+// Initialize Firebase Cloud Messaging and get a reference to the service
+const messaging = getMessaging(app);
 
-export { auth }
+const auth = getAuth(); // For Google Sign in
+
+export { auth, getToken, deleteToken, messaging }
